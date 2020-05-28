cd51769e9b7517920786c85ff2c93a4360ed1d52
sudhish <sudhishmishra824@gmail.com>
2020-05-29 04:00:17 +0530

    MERN application

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -26,12 +26,12 @@ class AppNavbar extends Component {
             <div>
             <Navbar color = "dark" dark expand = "sm" className = "mb-5">
                 <Container>
-                  <NavbarBrand href="/">ShoppingList</NavbarBrand>  
+                  <NavbarBrand href="/">Shopping List</NavbarBrand>  
                   <NavbarToggler onClick={this.toggle} />
                   <Collapse isOpen={this.state.isOpen} navbar>
                       <Nav className="ml-auto" navbar>
                           <NavItem>
-                              <NavLink href="https://github.com/sudhish">
+                              <NavLink href="https://github.com/Sudhish2101">
                                   GitHub
                               </NavLink>
                           </NavItem>
